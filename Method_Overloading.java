449e807e94568b1a175ba6084e5e0d57eea9ad38
Yogi288 <yogisri1998@gmail.com>
2022-12-22 10:32:33 +0530

    send

---
+++ Method_Overloading.java
@@ -0,0 +1,33 @@
+package com.polymorphism;
+
+public class Method_Overloading {
+
+	private void Student_Data(char name, String initial) {
+		System.out.println("Student Initial : " + name);
+		System.out.println("Student Name : " + initial);
+
+	}
+
+	private void Student_Data(int age) {
+		System.out.println("Student Age :" + age);
+	}
+
+	private void Student_Data(float height) {
+		System.out.println("Student Height : " + height);
+
+	}
+
+	private void Student_Data(boolean mark) {
+		System.out.println("Student Pass :" + mark);
+
+	}
+
+	public static void main(String[] args) {
+		Method_Overloading data = new Method_Overloading();
+		data.Student_Data('A', "Reena");
+		data.Student_Data(24);
+		data.Student_Data(5.96f);
+		data.Student_Data(true);
+	}
+
+}
